2f8e7fcedeb9f13805b3a3f1b6f9289b1f6b8f5f
Justin Lampe <pierrelampre@gmail.com>
2019-10-21 17:43:19 -0400

    refactored js/jQuery code to be much less repetitive.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,150 +14,60 @@
  $("#win").text(winCount);
  $("#lose").text(loseCount);
 
+$(".crystal").mouseup(function(){
+  if (matchingNumber === computerNumber) {
+    winCount++;
+    $("#win").text(winCount);
+     computerNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
+     $("#number-to-match").text(computerNumber);
+     crystal1_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
+     crystal2_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
+     crystal3_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
+     crystal4_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
+     matchingNumber = 0;
+     $("#user-number").text(matchingNumber);
+ }
 
+ if (matchingNumber > computerNumber) {
+   loseCount++
+   $("#lose").text(loseCount);
+   computerNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
+   $("#number-to-match").text(computerNumber);
+   crystal1_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
+   crystal2_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
+   crystal3_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
+   crystal4_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
+   matchingNumber = 0;
+   $("#user-number").text(matchingNumber);
+}
+});
 
- $("#crystal1").on("click", function(){
+ $("#crystal1").mousedown("click", function(){
     matchingNumber = crystal1_Number + matchingNumber;
-    
-    if (matchingNumber === computerNumber) {
-        winCount++;
-        $("#win").text(winCount);
-         computerNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
-         $("#number-to-match").text(computerNumber);
-         crystal1_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         crystal2_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         crystal3_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         crystal4_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         matchingNumber = 0;
-     }
-   
-     if (matchingNumber > computerNumber) {
-       loseCount++
-       $("#lose").text(loseCount);
-       computerNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
-       $("#number-to-match").text(computerNumber);
-       crystal1_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       crystal2_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       crystal3_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       crystal4_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       matchingNumber = 0;
-   }
     $("#user-number").text(matchingNumber);
   })
-  $("#crystal2").on("click", function(){
+  $("#crystal2").mousedown("click", function(){
     matchingNumber = crystal2_Number + matchingNumber;
-    if (matchingNumber === computerNumber) {
-        winCount++;
-        $("#win").text(winCount);
-         computerNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
-         $("#number-to-match").text(computerNumber);
-         crystal1_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         crystal2_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         crystal3_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         crystal4_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         matchingNumber = 0;
-     }
-   
-     if (matchingNumber > computerNumber) {
-       loseCount++;
-       $("#lose").text(loseCount);
-       computerNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
-       $("#number-to-match").text(computerNumber);
-       crystal1_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       crystal2_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       crystal3_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       crystal4_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       matchingNumber = 0;
-   }
     $("#user-number").text(matchingNumber);
   })
-  $("#crystal3").on("click", function(){
+  $("#crystal3").mousedown("click", function(){
     matchingNumber = crystal3_Number + matchingNumber;
-    if (matchingNumber === computerNumber) {
-        winCount++;
-        $("#win").text(winCount);
-         computerNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
-         $("#number-to-match").text(computerNumber);
-         crystal1_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         crystal2_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         crystal3_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         crystal4_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         matchingNumber = 0;
-     }
-   
-     if (matchingNumber > computerNumber) {
-       loseCount++;
-       $("#lose").text(loseCount);
-       computerNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
-       $("#number-to-match").text(computerNumber);
-       crystal1_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       crystal2_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       crystal3_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       crystal4_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       matchingNumber = 0;
-   }
     $("#user-number").text(matchingNumber);
   })
-  $("#crystal4").on("click", function(){
+  $("#crystal4").mousedown("click", function(){
     matchingNumber = crystal4_Number + matchingNumber;
-    if (matchingNumber === computerNumber) {
-        winCount++;
-        $("#win").text(winCount);
-         computerNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
-         $("#number-to-match").text(computerNumber);
-         crystal1_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         crystal2_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         crystal3_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         crystal4_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-         matchingNumber = 0;
-     }
-   
-     if (matchingNumber > computerNumber) {
-       loseCount++;
-       $("#lose").text(loseCount);
-       computerNumber = (Math.floor(Math.random() * (120 - 19 + 1) + 19));
-       $("#number-to-match").text(computerNumber);
-       crystal1_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       crystal2_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       crystal3_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       crystal4_Number = (Math.floor(Math.random() * (12 - 1 + 1) + 1));
-       matchingNumber = 0;
-   }
     $("#user-number").text(matchingNumber);
   })
 
-  $( "#crystal1" ).mousedown(function() {
-    document.getElementById("crystal1").style = "border: 1.5px solid rgb(126, 51, 248); box-shadow: 0 0 10px rgb(75, 15, 170);"
+  $( "#crystal1,#crystal2,#crystal3,#crystal4" ).mousedown(function() {
+    $(this).css("border", "1.5px solid rgb(126, 51, 248)"); 
+    $(this).css("box-shadow: 0 0 10px", "rgb(75, 15, 170)");
   });
 
-  $( "#crystal1" ).mouseup(function() {
-    document.getElementById("crystal1").style = " border: 1px solid white;"
+  $( "#crystal1,#crystal2,#crystal3,#crystal4" ).mouseup(function() {
+    $(this).css("border", "1px solid white");
   });
 
-  $( "#crystal2" ).mousedown(function() {
-    document.getElementById("crystal2").style = "border: 1.5px solid rgb(126, 51, 248); box-shadow: 0 0 10px rgb(75, 15, 170);"
-  });
-
-  $( "#crystal2" ).mouseup(function() {
-    document.getElementById("crystal2").style = " border: 1px solid white;"
-  });
-
-  $( "#crystal3" ).mousedown(function() {
-    document.getElementById("crystal3").style = "border: 1.5px solid rgb(126, 51, 248); box-shadow: 0 0 10px rgb(75, 15, 170);"
-  });
-
-  $( "#crystal3" ).mouseup(function() {
-    document.getElementById("crystal3").style = " border: 1px solid white;"
-  });
-
-  $( "#crystal4" ).mousedown(function() {
-    document.getElementById("crystal4").style = "border: 1.5px solid rgb(126, 51, 248); box-shadow: 0 0 10px rgb(75, 15, 170);"
-  });
-
-  $( "#crystal4" ).mouseup(function() {
-    document.getElementById("crystal4").style = " border: 1px solid white;"
-  });
-  
-
+ 
 
 
